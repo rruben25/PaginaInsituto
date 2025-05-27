f5bba85cdb8e13d84a6366033fd618b9c9c91962
rruben25 <rubenrat25@gmail.com>
2025-05-27 09:10:34 +0200

    PRUEBA-V4

--- reactasir/src/paginaWeb/Header.jsx
+++ reactasir/src/paginaWeb/Header.jsx
@@ -4,7 +4,7 @@ import style from './header.module.css'
 function Header() {
   return (
     <header className={style.cabecera}>
-        <h1>PRUEBA ACTUALIZACION V3</h1>
+        <h1>PRUEBA Github</h1>
     </header>
   )
 }
